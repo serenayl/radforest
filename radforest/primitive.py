30679bbe03a64d368eb5896a93f490837c1d40e4
serenayl <serena.li@wework.com>
2019-11-18 07:17:01 -0500

    AEC Tech Workshop

---
+++ radforest/primitive.py
@@ -0,0 +1,18 @@
+***
+primitive module.
+blah blah more documenting here
+***
+
+
+class Primitive(object):
+    ***
+    Primitive class
+
+    * to_radiance
+    * from_radiance
+    * to_json
+    * from_json
+    ***
+
+    def __init__(self):
+        pass
---
+++ radforest/setup.py
@@ -0,0 +1,14 @@
+from setup import setup
+
+setup(
+    name='fadforest',
+    version='0.1.0',
+    description='A library to interact with forests...',
+    url='https://github.com/serenayl/radforest',
+    author='me',
+    author_email='me@example.com',
+    license='MIT',
+    packages=['radforest'],
+    install_requires=[],
+    include_package_data=True
+)
